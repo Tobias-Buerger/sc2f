16012ca4bbd0bcbdf4a28386bd54d373af3a1c5f
Tobias Bürger <tobi-buerger@web.de>
2023-08-27 15:37:29 +0200

    project init

---
+++ README.md
@@ -0,0 +1,14 @@
+# sc2f - Stupid copy to favorite
+
+This program written in Rust makes a copy of an image file to the destination
+folder if made a favorite.
+
+## Usage
+
+Select source and destination folder and press "Go".
+
+Navigation:
+* Right arrow: next image
+* Left arrow: previous image
+* Up arrow: make favorite
+* Down arrow: remove favorite
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+use simple_logger;
+use log::*;
+
+/// Simple image viewer
+#[derive(Debug, Parser)]
+struct CliArgs {
+    /// Use one of those levels: Trace, Debug, Info, Warn, Error
+    #[arg(value_enum, short, long, default_value = "Warn")]
+    log_level: Level,
+}
+
+fn main() {
+    let args = CliArgs::parse();
+    simple_logger::init_with_level(args.log_level).unwrap();
+    info!("Program arguments processed");
+
+    
+}
